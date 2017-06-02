3e23bdfaa016a72c13a0e4d4af86306927342684
Muthiah Annamalai <ezhillang@gmail.com>
2017-06-01 22:31:36 -0700

    ubuntu install to take care of missing dirs

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -x
 
+if [ -d /usr/share/fonts/truetype ];
+then
+   #all good
+else
+   sudo mkdir -p  /usr/share/fonts/truetype
+fi
 sudo cp -vi ./fonts-installer/fontfiles/*Uni*   /usr/share/fonts/truetype/ 
 # should we do this instead ?
 # sudo cp -vi `find ./ | grep -i -e 'ttf$'` /usr/share/fonts/truetype/
