3ad5245cc7b8380971de2f4b0b0d964c439be1a3
Muthiah Annamalai <ezhillang@gmail.com>
2017-06-04 13:42:18 -0700

    correct if-then-else syntax

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -2,7 +2,7 @@
 
 if [ -d /usr/share/fonts/truetype ];
 then
-   #all good
+   echo OK #all good
 else
    sudo mkdir -p  /usr/share/fonts/truetype
 fi
@@ -10,4 +10,3 @@ sudo cp -vi ./fonts-installer/fontfiles/*Uni*   /usr/share/fonts/truetype/
 # should we do this instead ?
 # sudo cp -vi `find ./ | grep -i -e 'ttf$'` /usr/share/fonts/truetype/
 sudo fc-cache -f -v
-
